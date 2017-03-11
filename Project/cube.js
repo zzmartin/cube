fae78de166f5f938bba86da94208ac50a1914bea
zzmartin <martinwong1994@hotmail.com>
2017-03-10 18:43:20 -0600

    Add files via upload

---
+++ Project/cube.js
@@ -0,0 +1,117 @@
+var canvas;
+var gl;
+
+var NumVertices = 0;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [0, 0, 0];
+
+var thetaLoc;
+
+var cubeM = 3;
+var rotateV = 5;
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
+
+    var cube = new cubeModel();
+
+    for (var i = 0; i < cubeM; i++) {
+        for (var j = 0; j < cubeM; j++) {
+            for (var k = 0; k < cubeM; k++) {
+                console.log(i);
+                NumVertices += 36;
+                //cube.scale(1/2,1/2,1/2);
+                cube.scale(1 / cubeM, 1 / cubeM, 1 / cubeM);
+                cube.translate(-.5 + 1 / (2 * cubeM) + 1.1 * i / cubeM, -.5 + 1 / (2 * cubeM) + 1.1 * j / cubeM, -.5 + 1 / (2 * cubeM) + 1.1 * k / cubeM);
+                //  cube.translate(-1+(1/cubeM)/2,0,0);
+
+                this.points = this.points.concat(cube.points);
+
+                this.colors = this.colors.concat(cube.colors);
+
+                cube = new cubeModel();
+            }
+        }
+    }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    //event listeners for rotatetions 
+
+   
+    window.addEventListener("keydown", function() {
+console.log(event.keyCode);
+        switch (event.keyCode) {
+
+        case 38: case 87:
+            //rotate up
+            theta[0] +=rotateV;
+            break;
+       case 40: case 83:
+            //rotate down
+            theta[0] -=rotateV;
+            break;
+        case 37: case 65:
+            //rotate left
+            theta[1] +=rotateV;
+            break;
+        case 39: case 68:
+            //rotate right
+            theta[1] -=rotateV;
+            break;
+        }
+    });
+
+    render();
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    theta[axis] += 0.0;
+    gl.uniform3fv(thetaLoc, theta);
+
+    gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
+
+    requestAnimFrame(render);
+}
---
+++ Project/cubeModel.js
@@ -0,0 +1,93 @@
+"use strict";
+
+
+    
+function cubeModel()
+{
+    var result= {};
+    
+   var NumVertices  = 36;
+   var points = new Array();
+   var colors = new Array();
+   var vertices = new Array(
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5, -0.5, -0.5, 1.0 )
+    );
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+
+    colorCube();
+    result.points=points;
+    result.colors=colors;
+    result.NumVertices=NumVertices;
+    result.translate=translate;
+    result.scale=scale;
+
+    return result;
+    
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d) 
+{
+   
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+    
+    //vertex color assigned by the index of the vertex
+    
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        //colors.push( vertexColors[indices[i]] );
+    
+        // for solid colored faces use 
+        colors.push(vertexColors[a]);
+        
+    }
+}
+function translate(dx,dy,dz) //from skgCubeModel, lecture 2/22
+{
+    for(var i=0;i<8;i++)
+    {
+        vertices[i][0] += dx;
+        vertices[i][1] += dy;
+        vertices[i][2] += dz;
+
+    };
+}
+function scale(sx,sy,sz) //from skgCubeModel, lecture 2/22
+{
+    for(var i=0;i<8;i++)
+    {
+        vertices[i][0] *= sx;
+        vertices[i][1] *= sy;
+        vertices[i][2] *= sz;
+
+    };
+}
+}
